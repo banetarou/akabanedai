f235bcd67f3d392e129b0ba0de2b06920c78d0fc
banetarou <89698992+banetarou@users.noreply.github.com>
2025-10-29 22:41:02 +0900

    Restructure header navigation and update front page defaults

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -30,8 +30,42 @@ add_filter( 'body_class', 'achirabe_body_classes' );
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
 function achirabe_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
-	}
+        if ( is_singular() && pings_open() ) {
+                printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+        }
 }
 add_action( 'wp_head', 'achirabe_pingback_header' );
+
+/**
+ * Retrieves the public custom post types that should appear in the header navigation.
+ *
+ * @return array<WP_Post_Type> Array of custom post type objects.
+ */
+function achirabe_get_custom_post_type_objects() {
+        $post_types = get_post_types(
+                array(
+                        'public'   => true,
+                        '_builtin' => false,
+                ),
+                'objects'
+        );
+
+        if ( empty( $post_types ) ) {
+                return array();
+        }
+
+        return array_filter(
+                $post_types,
+                static function ( $post_type ) {
+                        if ( ! $post_type instanceof WP_Post_Type ) {
+                                return false;
+                        }
+
+                        if ( empty( $post_type->has_archive ) ) {
+                                return false;
+                        }
+
+                        return (bool) get_post_type_archive_link( $post_type->name );
+                }
+        );
+}
