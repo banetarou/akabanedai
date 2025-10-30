ed1e837ac966c84cdd8223985552ca4c32413c04
banetarou <89698992+banetarou@users.noreply.github.com>
2025-10-30 10:13:25 +0900

    Customize underscores theme for minimal gallery layout

--- index.php
+++ index.php
@@ -3,31 +3,48 @@
  * Index template displaying the vertical gallery of posts.
  */
 get_header();
+?>
 
-if ( have_posts() ) :
-    // Loop through posts and render gallery content.
-    while ( have_posts() ) :
-        the_post();
-        ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class( 'gallery-item' ); ?>>
-            <?php if ( has_post_thumbnail() ) : ?>
-                <div class="gallery-image">
-                    <?php the_post_thumbnail( 'large' ); ?>
-                </div>
-            <?php endif; ?>
-            <div class="gallery-caption">
-                <h2 class="gallery-title"><?php the_title(); ?></h2>
+<section class="gallery-list">
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) :
+            the_post();
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class( 'gallery-item' ); ?>>
+                <?php if ( has_post_thumbnail() ) : ?>
+                    <figure class="gallery-media">
+                        <?php the_post_thumbnail( 'large' ); ?>
+                    </figure>
+                <?php endif; ?>
+
+                <header class="gallery-header">
+                    <h2 class="gallery-title">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h2>
+                </header>
+
                 <div class="gallery-description">
                     <?php the_content(); ?>
+                    <?php
+                    wp_link_pages(
+                        array(
+                            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'minimal-gallery' ),
+                            'after'  => '</div>',
+                        )
+                    );
+                    ?>
                 </div>
-            </div>
-        </article>
+            </article>
+            <?php
+        endwhile;
+    else :
+        ?>
+        <p class="no-entries"><?php esc_html_e( 'No works available at the moment.', 'minimal-gallery' ); ?></p>
         <?php
-    endwhile;
-else :
+    endif;
     ?>
-    <p><?php esc_html_e( 'No works available at the moment.', 'minimal-gallery' ); ?></p>
-    <?php
-endif;
+</section>
 
+<?php
 get_footer();
