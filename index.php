bc49c7120b73150b1fc31e818d234e59732b3a76
Masahiko Suda <masahiko_suda@hotmail.com>
2025-10-30 15:27:24 +0900

    Display works tiles on the home page

--- index.php
+++ index.php
@@ -18,34 +18,79 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php
-		if ( have_posts() ) :
+		if ( is_front_page() || is_home() ) :
+			$works_query = new WP_Query(
+				array(
+					'post_type'      => 'works',
+					'posts_per_page' => -1,
+					'post_status'    => 'publish',
+				)
+			);
 
-			if ( is_home() && ! is_front_page() ) :
+			if ( $works_query->have_posts() ) :
 				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+				<div class="works-grid">
+					<?php
+					while ( $works_query->have_posts() ) :
+						$works_query->the_post();
+						?>
+						<article class="works-grid__item">
+							<a
+								class="works-grid__link"
+								href="<?php the_permalink(); ?>"
+								aria-label="<?php echo esc_attr( get_the_title() ); ?>"
+							>
+								<?php
+								if ( has_post_thumbnail() ) {
+									the_post_thumbnail(
+										'large',
+										array(
+											'class' => 'works-grid__image',
+										)
+									);
+								} else {
+									printf( '<span class="works-grid__placeholder" aria-hidden="true"></span>' );
+								}
+								?>
+							</a>
+						</article>
+						<?php
+					endwhile;
+					?>
+				</div>
 				<?php
+			else :
+				get_template_part( 'template-parts/content', 'none' );
 			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
+			wp_reset_postdata();
 
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+			if ( have_posts() ) :
+
+				if ( is_home() && ! is_front_page() ) :
+					?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+					<?php
+				endif;
+
+				while ( have_posts() ) :
+					the_post();
+
+					get_template_part( 'template-parts/content', get_post_type() );
+
+				endwhile;
+
+				the_posts_navigation();
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif;
 
 		endif;
 		?>
