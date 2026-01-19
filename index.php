23207110b146daf45057deae09ecce058f46b97c
Masahiko Suda <masahiko_suda@hotmail.com>
2026-01-19 16:09:09 +0900

    Remove sidebar from index template

--- index.php
+++ index.php
@@ -19,51 +19,89 @@ get_header();
 
 		<?php
 		if ( is_front_page() || is_home() ) :
-			$works_query = new WP_Query(
-				array(
-					'post_type'      => 'works',
-					'posts_per_page' => -1,
-					'post_status'    => 'publish',
-				)
-			);
+			$top_page    = get_page_by_path( 'top' );
+			$top_content = '';
 
-			if ( $works_query->have_posts() ) :
+			if ( $top_page ) {
+				$top_content = apply_filters( 'the_content', $top_page->post_content );
+			}
+
+			$top_images = array();
+
+			if ( ! empty( $top_content ) ) {
+				libxml_use_internal_errors( true );
+				$dom = new DOMDocument();
+				$dom->loadHTML( mb_convert_encoding( $top_content, 'HTML-ENTITIES', 'UTF-8' ) );
+				$xpath = new DOMXPath( $dom );
+				$nodes = $xpath->query( '//img' );
+
+				foreach ( $nodes as $image_node ) {
+					$src = $image_node->getAttribute( 'src' );
+
+					if ( empty( $src ) ) {
+						continue;
+					}
+
+					$alt = $image_node->getAttribute( 'alt' );
+
+					$link_node = $image_node->parentNode;
+					$link_href = '';
+					$link_aria = '';
+
+					if ( $link_node && 'a' === $link_node->nodeName ) {
+						$link_href = $link_node->getAttribute( 'href' );
+						$link_aria = $link_node->getAttribute( 'aria-label' );
+
+						if ( empty( $link_aria ) ) {
+							$link_aria = trim( $link_node->textContent );
+						}
+					}
+
+					if ( empty( $link_aria ) ) {
+						$link_aria = $alt;
+					}
+
+					if ( empty( $link_href ) ) {
+						$link_href = $src;
+					}
+
+					$top_images[] = array(
+						'src'  => $src,
+						'alt'  => $alt,
+						'href' => $link_href,
+						'aria' => $link_aria,
+					);
+				}
+				libxml_clear_errors();
+			}
+
+			if ( ! empty( $top_images ) ) :
 				?>
 				<div class="works-grid">
 					<?php
-					while ( $works_query->have_posts() ) :
-						$works_query->the_post();
+					foreach ( $top_images as $top_image ) :
 						?>
 						<article class="works-grid__item">
 							<a
 								class="works-grid__link"
-								href="<?php the_permalink(); ?>"
-								aria-label="<?php echo esc_attr( get_the_title() ); ?>"
+								href="<?php echo esc_url( $top_image['href'] ); ?>"
+								aria-label="<?php echo esc_attr( $top_image['aria'] ); ?>"
 							>
-								<?php
-								if ( has_post_thumbnail() ) {
-									the_post_thumbnail(
-										'large',
-										array(
-											'class' => 'works-grid__image',
-										)
-									);
-								} else {
-									printf( '<span class="works-grid__placeholder" aria-hidden="true"></span>' );
-								}
-								?>
+								<img
+									src="<?php echo esc_url( $top_image['src'] ); ?>"
+									class="works-grid__image wp-post-image"
+									alt="<?php echo esc_attr( $top_image['alt'] ); ?>"
+								>
 							</a>
 						</article>
 						<?php
-					endwhile;
+					endforeach;
 					?>
 				</div>
 				<?php
 			else :
 				get_template_part( 'template-parts/content', 'none' );
 			endif;
-
-			wp_reset_postdata();
 
 		else :
 
@@ -98,5 +136,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
