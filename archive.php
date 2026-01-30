ebc65b0fa909fab205a7aa1d1856a8732504460d
Masahiko Suda <masahiko_suda@hotmail.com>
2026-01-30 11:45:36 +0900

    Redirect blog singles to archive

--- archive.php
+++ archive.php
@@ -14,14 +14,16 @@ get_header();
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-				if ( ! is_tax() ) {
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				}
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			<?php if ( ! is_post_type_archive( 'blog' ) ) : ?>
+				<header class="page-header">
+					<?php
+					if ( ! is_tax() ) {
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+					}
+					the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
+			<?php endif; ?>
 
 			<?php
 			if ( is_post_type_archive( array( 'works', 'projects' ) ) || is_tax( array( 'work_category', 'project_category' ) ) ) :
