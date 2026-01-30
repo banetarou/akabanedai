4147a01fe91ad27253f3a2aae544bc2d69c196a9
Masahiko Suda <masahiko_suda@hotmail.com>
2026-01-30 11:18:38 +0900

    Remove archive title heading on tax pages

--- archive.php
+++ archive.php
@@ -16,7 +16,9 @@ get_header();
 
 			<header class="page-header">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				if ( ! is_tax() ) {
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+				}
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
